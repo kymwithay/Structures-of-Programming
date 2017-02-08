93e167f27ee97ccf9ba1ae80158d4afae379579a
kymwithay <hillkymberlee@gmail.com>
2017-02-08 13:46:59 -0500

    checking for non-numeric numbers

--- Logic-Programming-Assignment/lp.pl
+++ Logic-Programming-Assignment/lp.pl
@@ -12,3 +12,8 @@ sum-up-numbers-simple([X|Y], N);- 	%predicate that takes a list and gives result
 number(X), 					 		%if head is a number 
 sum-up-numbers-simple(Y, N1), 		%recursive call to continue through list
 N is X + N1.						%assign sum to result
+
+
+sum-up-numbers-simple([X|Y], N):-   %Base Case: Checks for non-numeric numbers
+\+number(H),                        %if its not a number recusive call next element.
+sum-up-numbers-simple(Y, N).
