2c36e11aa2a571cde0a24ec8b209afa0e0db6cd4
kymwithay <hillkymberlee@gmail.com>
2017-02-08 02:15:30 -0500

    changed file extension and added list facts

---
+++ Logic-Programming-Assignment/lp.pl
@@ -0,0 +1,12 @@
+Kymberlee Hill
+@02752656
+Structures of Programming 
+
+Logic Programming Assignment
+
+*/Question 1
+
+sum-up-numbers-simple(L,N);-
+contains(L, non numbers)
+contains(L, numbers)
+N is X + Y.
