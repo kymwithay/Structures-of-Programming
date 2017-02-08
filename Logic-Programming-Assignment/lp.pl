30cef6f52346366f14940088943217365de136dd
kymwithay <hillkymberlee@gmail.com>
2017-02-08 13:43:14 -0500

    made the predicate take a list as a parameter

--- Logic-Programming-Assignment/lp.pl
+++ Logic-Programming-Assignment/lp.pl
@@ -4,14 +4,11 @@ Structures of Programming
 
 Logic Programming Assignment
 
-%Question 1
 
-%Functions that are being used as helper function declarations
-sum-up-numbers-simple([],0). %Base Case when the list is empty
+/* Question1 */
+sum-up-numbers-simple([],0). %Base Case: Check if list is empty then 0
 
-%base casefor numberic numbers
-sum-up-numbers-simple(L,N);- %the sum of numples in simple list is N if & only if 
-[X|Y] = L, %X is the head of L list & Y is the tail of L list
-number(X), %and X is a number 
-sum-up-numbers-simple(Y, N1),
-N is X + N1.
+sum-up-numbers-simple([X|Y], N);- 	%predicate that takes a list and gives result 
+number(X), 					 		%if head is a number 
+sum-up-numbers-simple(Y, N1), 		%recursive call to continue through list
+N is X + N1.						%assign sum to result
