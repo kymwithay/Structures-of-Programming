c9087a18e00d3624819d12b09fded7a5d8883b32
kymwithay <hillkymberlee@gmail.com>
2017-02-08 13:34:31 -0500

    changed N to N1 variable in predicate and arithmetic logic

--- Logic-Programming-Assignment/lp.pl
+++ Logic-Programming-Assignment/lp.pl
@@ -9,9 +9,9 @@ Logic Programming Assignment
 %Functions that are being used as helper function declarations
 sum-up-numbers-simple([],0). %Base Case when the list is empty
 
-%for numberic numbers
+%base casefor numberic numbers
 sum-up-numbers-simple(L,N);- %the sum of numples in simple list is N if & only if 
 [X|Y] = L, %X is the head of L list & Y is the tail of L list
 number(X), %and X is a number 
-sum-up-numbers-simple(Y, N),
-N is X + Y.
+sum-up-numbers-simple(Y, N1),
+N is X + N1.
